dfa27285d448972ce2a55ab1cd2dd3b7920041fd
Alex Laird <alexdlaird@gmail.com>
2018-04-22 11:19:00 -0700

    Mocking copyright_name.

--- tests/testactions.py
+++ tests/testactions.py
@@ -11,9 +11,10 @@ __version__ = '1.1.1'
 
 
 class TestActionsTestCase(unittest.TestCase):
+    @mock.patch('heliumcli.actions.update.utils.get_copyright_name', return_value='Helium Edu')
     @mock.patch('heliumcli.actions.update.git.Repo', return_value=repo_stub)
     @mock.patch('heliumcli.actions.update.subprocess.call')
-    def test_update(self, mock_call, mock_repo):
+    def test_update(self, mock_call, mock_repo, mock_copyright_name):
         main(['main.py', 'update'])
 
         self.assertTrue(mock_repo.called)
