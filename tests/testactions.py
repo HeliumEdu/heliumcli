2efb9d6dff2318cfdfca6cfac5f589466a9957f7
Alex Laird <alexdlaird@gmail.com>
2018-04-22 15:36:36 -0700

    Adding test for clone/update edge case.

--- tests/testactions.py
+++ tests/testactions.py
@@ -1,5 +1,7 @@
 import os
 
+from mock import mock
+
 from .helpers.testcase import HeliumCLITestCase
 from ..actions import utils
 from ..main import main
@@ -17,7 +19,17 @@ class TestActionsTestCase(HeliumCLITestCase):
         self.mock_subprocess_call.assert_called_once_with(
             ['pip', 'install', '-r', os.path.join(utils.get_heliumcli_dir(), "requirements.txt")])
 
-    def test_update_projects(self):
+    def test_update_clone_projects(self):
+        main(['main.py', 'update-projects'])
+
+        self.assertEqual(self.mock_git_repo.clone_from.call_count, 2)
+        self.mock_subprocess_call.assert_any_call(
+            ['make', 'install', '-C', os.path.join(utils.get_projects_dir(), "platform")])
+        self.mock_subprocess_call.assert_any_call(
+            ['make', 'install', '-C', os.path.join(utils.get_projects_dir(), "frontend")])
+
+    @mock.patch('os.path.exists', return_value=True)
+    def test_update_projects(self, mock_path_exists):
         main(['main.py', 'update-projects'])
 
         self.assertEqual(self.mock_git_repo.return_value.git.pull.call_count, 3)
