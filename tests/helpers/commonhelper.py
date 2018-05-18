f20ab6c2795c8ece143b9b126d872743ced68eba
Alex Laird <alexdlaird@gmail.com>
2018-05-17 21:47:09 -0700

    no message

--- tests/helpers/commonhelper.py
+++ tests/helpers/commonhelper.py
@@ -9,11 +9,8 @@ __version__ = '1.1.3'
 
 
 def given_runserver_exists(project):
-    if not os.path.exists(utils.get_projects_dir()):
-        os.mkdir(utils.get_projects_dir())
-
     if not os.path.exists(os.path.join(utils.get_projects_dir(), project)):
-        os.mkdir(os.path.join(utils.get_projects_dir(), project))
+        os.makedirs(os.path.join(utils.get_projects_dir(), project))
 
     os.mkdir(os.path.join(utils.get_projects_dir(), project, os.path.dirname(utils.get_config()["serverBinFilename"])))
     open(os.path.join(utils.get_projects_dir(), project, utils.get_config()["serverBinFilename"]), "wb").close()
