3a992387c9c57688a2dffb6fab499f45fef3b024
Alex Laird <alexdlaird@gmail.com>
2018-11-01 15:12:03 -0700

    Finish implementation of initing template project.

--- heliumcli/actions/init.py
+++ heliumcli/actions/init.py
@@ -1,5 +1,10 @@
 import os
+import shutil
+import subprocess
 import sys
+from distutils.dir_util import copy_tree
+
+import git
 
 from .. import utils
 
@@ -18,7 +23,7 @@ class InitAction:
         parser.add_argument("id", help="The ID (no spaces) to give to the new project")
         parser.add_argument("name", help="The friendly name to give to the new project")
         parser.add_argument("host", help="The hostname to give the project")
-        parser.add_argument("github-user", help="The GitHub username or project name")
+        parser.add_argument("github_user", help="The GitHub username or project name")
         parser.set_defaults(action=self)
 
     def run(self, args):
@@ -35,5 +40,41 @@ class InitAction:
         print("A new helium-cli project has been initialized.")
 
     def _init_project(self, args):
-        # TODO: implement template init
-        pass
+        template_project_name = "template-project"
+
+        print("Cloning the template-project repo into this directory ...")
+        git.Repo.clone_from("{}/{}.git".format("https://github.com/HeliumEdu", template_project_name),
+                            template_project_name)
+
+        copy_tree(template_project_name, ".")
+        shutil.rmtree(template_project_name)
+        shutil.rmtree(".git")
+
+        repo = git.Repo.init(".")
+
+        print("Updating template variables ...")
+
+        upper_slug = args.id.replace("-", "_").replace(" ", "_").upper()
+        lower_slug = upper_slug.lower()
+
+        for dir_name, dirs, files in os.walk("."):
+            for filename in files:
+                path = os.path.join(dir_name, filename)
+                with open(path, "r") as f:
+                    s = f.read()
+                s = s.replace("{%PROJECT_ID%}", args.id)
+                s = s.replace("{%PROJECT_ID_UPPER%}", upper_slug)
+                s = s.replace("{%PROJECT_ID_LOWER%}", lower_slug)
+                s = s.replace("{%PROJECT_NAME%}", args.name)
+                s = s.replace("{%PROJECT_GITHUB_USER%}", args.github_user)
+
+                with open(path, "w") as f:
+                    f.write(s)
+
+        repo.git.add(A=True)
+
+        os.rename("project_id", lower_slug)
+
+        print("Running make ...")
+
+        subprocess.call(["make", "install"])
