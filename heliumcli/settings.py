61e114bff4d4ebaacd9b941fe58c1e3c44239af7
Alex Laird <alexdlaird@gmail.com>
2020-06-21 16:20:43 -0700

    Update settings.py

--- heliumcli/settings.py
+++ heliumcli/settings.py
@@ -2,8 +2,8 @@ import json
 import os
 
 __author__ = "Alex Laird"
-__copyright__ = "Copyright 2019, Helium Edu"
-__version__ = "1.5.12"
+__copyright__ = "Copyright 2020, Helium Edu"
+__version__ = "1.5.13"
 
 VERSION = __version__
 
