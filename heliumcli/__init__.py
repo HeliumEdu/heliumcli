ee5235b95dc15c3d20d6487ba3c77d47106db93f
Alex Laird <contact@alexlaird.com>
2024-10-05 17:48:17 -0500

    Remove dependency on Ansible to prep release code.

--- heliumcli/__init__.py
+++ heliumcli/__init__.py
@@ -1,3 +1,3 @@
 __copyright__ = "Copyright (c) 2018 Helium Edu"
 __license__ = "MIT"
-__version__ = "1.6.25"
+__version__ = "1.6.26"
--- heliumcli/utils.py
+++ heliumcli/utils.py
@@ -96,9 +96,12 @@ def should_update(line, verification, start_needle, end_needle=""):
 
 
 def get_copyright_name():  # pragma: no cover
-    with open(os.path.join(get_ansible_dir(), "group_vars", "all.yml"), "r") as lines:
-        data = yaml.safe_load(lines)
-        return data[get_config()["ansibleCopyrightNameVar"]]
+    if "copyrightName" in get_config():
+        return get_config("copyrightName")
+    else:
+        with open(os.path.join(get_ansible_dir(), "group_vars", "all.yml"), "r") as lines:
+            data = yaml.safe_load(lines)
+            return data[get_config()["ansibleCopyrightNameVar"]]
 
 
 def get_repo_name(repo_dir, remote_name):  # pragma: no cover
